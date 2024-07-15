a8668e6aad668833df7ed0dad5d2e352cfc265b9
Yoni Audi <yoniaudi@gmail.com>
2024-07-15 16:36:58 +0300

    IValidationStrategy.cs refactor done

--- FacebookWinFormsApp/Features/ValidationStrategy/IValidationStrategy.cs
+++ FacebookWinFormsApp/Features/ValidationStrategy/IValidationStrategy.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BasicFacebookFeatures.Features.ValidationStrategy
+﻿namespace BasicFacebookFeatures.Features.ValidationStrategy
 {
     public interface IValidationStrategy<T>
     {
-        bool Validate(T data, out string errorMessage);
+        bool Validate(T i_Data, out string o_ErrorMessage);
     }
 }
